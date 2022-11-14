5fa4794e1fd18434cc3cc6eb1c7835f2a2391ec0
gallegorh <gallegorh@hotmail.com>
2022-11-14 14:57:14 -0500

    Agregar archivos de proyecto.

---
+++ ManejoPresupuesto/Controllers/CuentasController.cs
@@ -0,0 +1,34 @@
+﻿
+using ManejoPresupuesto.Models;
+using ManejoPresupuesto.Servicios;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ManejoPresupuesto.Controllers
+{
+    public class CuentasController : Controller
+    {
+        private readonly IRepositorioTiposCuentas repositorioTiposCuentas;
+        private readonly IServicioUsuarios servicioUsuarios;
+
+        public CuentasController(IRepositorioTiposCuentas repositorioTiposCuentas, IServicioUsuarios servicioUsuarios)
+        {
+            this.RepositorioTiposCuentas = repositorioTiposCuentas;
+            this.ServicioUsuarios = servicioUsuarios;
+        }
+
+        public IRepositorioTiposCuentas RepositorioTiposCuentas { get; }
+        public IServicioUsuarios ServicioUsuarios { get; }
+
+        [HttpGet]
+        public async Task<IActionResult> Crear()
+        {
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            var tiposCuentas = await repositorioTiposCuentas.Obtener(usuarioId);
+            var modelo = new CuentaCreacionViewModel();
+            modelo.TiposCuentas = tiposCuentas.Select(x => new SelectListItem(x.Nombre, x.Id.ToString()));
+            return View(modelo); 
+        }
+        
+    }
+}
---
+++ ManejoPresupuesto/Models/TipoCuenta.cs
@@ -0,0 +1,29 @@
+﻿using ManejoPresupuesto.Validaciones;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace ManejoPresupuesto.Models
+{
+    public class TipoCuenta //: IValidatableObject
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="El campo {0} es requerido")]
+        [Display(Name ="Nombre del tipo cuenta")]
+        [PrimeraLetraMayuscula]
+        [Remote(action: "VerificarExisteTipoCuentas", controller: "TiposCuentas")]
+        public string Nombre { get; set; }
+        public int UsuarioId { get; set; }
+        public int Orden { get; set; }
+
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //    if(Nombre != null || Nombre.Length > 0)
+        //    { 
+        //        var primeraLetra = Nombre[0].ToString();
+
+        //        if (primeraLetra != primeraLetra.ToUpper()) { yield return new ValidationResult("La primera letra no es mayúscula", new[] {nameof(Nombre)}); }
+        //    }
+        //}
+    }
+}
